7422d1198e219611dff597a8a8aeb58b08453301
Hamid-0 <habol085@uottawa.ca>
2021-07-17 12:53:24 -0400

    changed some colours

--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -3,17 +3,21 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The Dojo Blog</h1>
+      <h1>AllCooks.com</h1>
       <div className="links">
         <Link to="/">Home</Link>
-        <Link to="/create" style={{ 
-          color: 'white', 
-          backgroundColor: '#f1356d',
-          borderRadius: '8px' 
-        }}>New Blog</Link>
+        <Link to="/explore" style={{
+          backgroundColor: '#ffba24',
+          borderRadius: '8px'
+        }}>Explore</Link>
+        <Link to="/favourites">Favourites</Link>
+        <Link to="/videos">Videos</Link>
+        <Link to="/faq">FAQ</Link>
+        <Link to="/faq">Contact Us</Link>
       </div>
+
     </nav>
   );
 }
- 
+
 export default Navbar;
